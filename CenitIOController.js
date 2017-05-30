61a68fa80ce67816e9e8c0aeeda03db8695f0d1b
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2017-05-30 16:51:35 -0400

    fix first integration

--- CenitIOController.js
+++ CenitIOController.js
@@ -2,6 +2,231 @@
  * SpreadsheetConverter nodejs helper module to persist calculator node form in CenitIO
  */
 
+var util = require('util'),
+    request = require('request');
+
 module.exports = {
-    //TODO: Implement helper actions.
+
+    /**
+     * Init process to send form data to CenitIO platform.
+     *
+     * @param formData {Object} Form data to be seved.
+     * @param CenitIO {Object} CenitIO connection setting.
+     * @param callback {Function} Callback function with error and response message parameters.
+     */
+    saveFormData: function (formData, CenitIO, callback) {
+        var vThis = this;
+
+        CenitIO = CenitIO || {};
+        formData = this.parseData(formData);
+
+        vThis.validate(CenitIO, function (err) {
+            if (err) return callback(err);
+
+            var uaKey = CenitIO.userAccessKey.trim(),
+                uaToken = CenitIO.userAccessToken.trim(),
+                dtName = CenitIO.dataTypeName.trim(),
+                dtNamespace = CenitIO.dataTypeNamespace.trim(),
+                baUrl = CenitIO.baseApiUrl.trim().replace(/\/$/, '');
+
+            vThis.getDataType(baUrl, uaKey, uaToken, dtNamespace, dtName, function (err, dataType) {
+                if (err) return callback(err);
+                if (dataType) {
+                    vThis.saveDataInDataType(baUrl, uaKey, uaToken, dataType, formData, callback);
+                } else {
+                    vThis.createDataType(baUrl, uaKey, uaToken, dtNamespace, dtName, formData, function (err, dataType) {
+                        if (err) return callback(err);
+                        vThis.saveDataInDataType(baUrl, uaKey, uaToken, dataType, formData, callback);
+                    });
+                }
+            });
+        });
+    },
+
+    /**
+     * Send form data to CenitIO platform.
+     *
+     * @param baUrl {String} Base URL to CenitIO API.
+     * @param uaKey {String} CenitIO user access key.
+     * @param uaToken {String} CenitIO user access token.
+     * @param dataType {Object} Data type record.
+     * @param formData {Object} Form data to be seved.
+     * @param callback {Function} Callback function with error and data type record record parameters.
+     */
+    saveDataInDataType: function (baUrl, uaKey, uaToken, dataType, formData, callback) {
+        var vThis = this,
+            options = {
+            url: util.format('%s/%s/%s.json', baUrl, dataType.namespace.toLowerCase(), dataType.name),
+            headers: this.headers(uaKey, uaToken),
+            method: 'POST',
+            json: true,
+            body: formData
+        };
+
+        request(options, function (err, response, resData) {
+            var msg, status;
+
+            if (err || resData.summary) {
+                msg = err.toString() || resData.summary;
+                status = 500;
+            } else {
+                msg = 'Data was successfully saved.';
+                status = 422;
+            }
+
+            callback(status, vThis.renderView(status, msg));
+        });
+    },
+
+    /**
+     * Search and return data type record with given name and namespace.
+     *
+     * @param baUrl {String} Base URL to CenitIO API.
+     * @param uaKey {String} CenitIO user access key.
+     * @param uaToken {String} CenitIO user access token.
+     * @param dtNamespace {String} Data type namespace.
+     * @param dtName {String} Data type name.
+     * @param callback {Function} Callback function with error and data type record record parameters.
+     */
+    getDataType: function (baUrl, uaKey, uaToken, dtNamespace, dtName, callback) {
+        var options = {
+            url: util.format('%s/setup/json_data_type.json', baUrl),
+            headers: this.headers(uaKey, uaToken),
+            method: 'GET',
+            json: true,
+            qs: { limit: 1, namespace: dtNamespace, name: dtName }
+        };
+
+        request(options, function (err, response, resData) {
+            if (err || resData.summary) return callback(err || resData.summary);
+            callback(null, resData.json_data_types[0])
+        });
+    },
+
+    /**
+     *
+     * @param baUrl {String} Base URL to CenitIO API.
+     * @param uaKey {String} CenitIO user access key.
+     * @param uaToken {String} CenitIO user access token.
+     * @param dtNamespace {String} Data type namespace.
+     * @param dtName {String} Data type name.
+     * @param formData {Object} Form data to be seved.
+     * @param callback
+     */
+    createDataType: function (baUrl, uaKey, uaToken, dtNamespace, dtName, formData, callback) {
+        var options = {
+            url: util.format('%s/setup/json_data_type.json', baUrl),
+            headers: this.headers(uaKey, uaToken),
+            method: 'POST',
+            json: true,
+            body: {
+                namespace: dtNamespace,
+                name: dtName,
+                schema: this.parseJsonSchema(formData)
+            }
+        };
+
+        request(options, function (err, response, resData) {
+            if (err || resData.summary) return callback(err || resData.summary);
+            callback(null, resData.success.json_data_type);
+        });
+    },
+
+    /**
+     * Parse json schema from formData.
+     *
+     * @param formData {Object} Form data to be seved.
+     * @returns {{type: string, properties: { field1: { type: "string" ... } } } }
+     */
+    parseJsonSchema: function (formData) {
+        var schema = { type: 'object', properties: {} };
+
+        Object.keys(formData).forEach(function (key) {
+            schema.properties[key] = { type: this.parseType(formData[key]) };
+        }, this);
+
+        return schema;
+    },
+
+    /**
+     * Parse data fields from formData.
+     *
+     * @param formData {Object} Form data to be seved.
+     * @returns {Object}
+     */
+    parseData: function (formData) {
+        var item = {}
+
+        Object.keys(formData).forEach(function (key) {
+            if (!key.match(/^xl_/)) item[key] = this.parseValue(formData[key]);
+        }, this);
+
+        return item;
+    },
+
+    /**
+     * Parse type from given value.
+     *
+     * @param value {*}
+     * @returns {string}
+     */
+    parseType: function (value) {
+        // TODO: Parse field value type.
+        return 'string'
+    },
+
+    /**
+     * Parse value in real type from given string value.
+     *
+     * @param value {string}
+     * @returns {*}
+     */
+    parseValue: function (value) {
+        // TODO: Parse field value.
+        return value
+    },
+
+    /**
+     * Validate CenitIO connection setting.
+     *
+     * @param CenitIO {Object} CenitIO connection setting.
+     * @param callback {Function} Callback function with error parameter.
+     */
+    validate: function (CenitIO, callback) {
+        var errMsg = 'CenitIO.%s is a required string, please set it to the json file.',
+            isValid = function (v) {
+                return typeof v == 'String' && v.trim() != ''
+            };
+
+        if (isValid(CenitIO.userAccessKey)) return callback(util.format(errMsg, 'userAccessKey'));
+        if (isValid(CenitIO.userAccessToken)) return callback(util.format(errMsg, 'userAccessToken'));
+        if (isValid(CenitIO.dataType)) return callback(util.format(errMsg, 'dataType'));
+        if (isValid(CenitIO.baseApiUrl)) return callback(util.format(errMsg, 'baseApiUrl'));
+
+        callback()
+    },
+
+    /**
+     * Returns headers to be sent in CenitIO request.
+     *
+     * @param uaKey {String} CenitIO user access key.
+     * @param uaToken {String} CenitIO user access token.
+     * @returns {{Content-Type: string, X-User-Access-Key: *, X-User-Access-Token: *}}
+     */
+    headers: function (uaKey, uaToken) {
+        return {
+            'Content-Type': 'application/json',
+            'X-User-Access-Key': uaKey,
+            'X-User-Access-Token': uaToken
+        }
+    },
+
+    renderView: function (status, msg) {
+        // TODO: Customise view.
+        var tmpl = '' +
+            '<div style="border: 1px solid gray; background-color: %s; padding: 1.5em; text-align: center;">%s</div>' +
+            '<script type="text/javascript">setTimeout(function () { window.location = "/" }, 5000)</script>';
+
+        return util.format(tmpl, status == 500 ? 'red' : '#c9e2b3', msg);
+    }
 };
--- README.md
+++ README.md
@@ -2,12 +2,55 @@
 
 [![N|Solid](http://www.spreadsheetconverter.com/wp-content/uploads/2013/08/logo.png)](http://www.spreadsheetconverter.com)
 
-CenitIOController is a helper module for SpreadsheetConverter Node.js calculator app to save your form submissions into 
+CenitSpreadsheetConverter is a helper module for SpreadsheetConverter Node.js calculator app to save your form submissions into 
 your [CenitIO Tenant](https://cenit.io/). With a small code manipulation inside the calculator app, you can easily connect 
 your form to your CenitIO Tenant.
 
 ### Installation
 
+Install cenit-spreadsheet-converter in your app by typing following in command prompt.
+
+```sh
+npm install cenit-io/cenit-spreadsheet-converter --save
+```
 
 ### Usage
 
+Add CenitIO connection setting to 'config.js' file. Open 'config.js' in your editor and customize this snippet code:
+
+```json
+{
+  "port": 3002,
+  "lcu": "test@g-forward.com",
+  "source": "anywhere",
+  "page": {
+    "description": "",
+    "error": "",
+    "username": "",
+    "password": "",
+    "login": "",
+    "message": ""
+  },
+  "CenitIO": {
+    "baseApiUrl": "https://cenit.io/api/v2",
+    "dataTypeName": "spreadsheet_test",
+    "dataTypeNamespace": "Basic",
+    "userAccessKey": "**********",
+    "userAccessToken": "********************"
+  }
+}
+```
+
+Include CenitIOController in your Node.js application folder. Now you need to edit app.js to handle the form save 
+post request from browser. Open app.js in your editor and paste this snippet code:
+
+
+```javascript
+var CenitIOController = require('cenit-spreadsheet-converter');
+
+app.post('/postform', function(req, res){
+    CenitIOController.saveFormData(req.body, config.CenitIO, function (status, msg) {
+        res.status(status).send(msg);
+    });
+});
+```
